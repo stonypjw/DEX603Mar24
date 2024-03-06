dc794236e02e8d37f3454a130323a54f6f970727
stonypjw <jeremy.white@stonyp.com>
2024-03-06 10:13:04 -0800

    addFilter

--- force-app/main/default/lwc/opportunityList/opportunityList.js
+++ force-app/main/default/lwc/opportunityList/opportunityList.js
@@ -1,4 +1,4 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import getOpportunities from '@salesforce/apex/OpportunityController.getOpportunities';
 
 export default class OpportunityList extends LightningElement {
@@ -10,6 +10,14 @@ export default class OpportunityList extends LightningElement {
     displayedOpps = [];
     allOpps = [];
     status = 'All';
+
+    @track comboOptions = [
+        {value: 'All', label: 'All' },
+        {value: 'Open', label: 'Open' },
+        {value: 'Closed', label: 'Closed' },
+        {value: 'ClosedWon', label: 'Closed Won' },
+        {value: 'ClosedLost', label: 'Closed Lost' }
+     ];
 
     @wire(getOpportunities, { accountId: '$recordId' })
     wiredOpps(oppRecords){
@@ -30,9 +38,42 @@ export default class OpportunityList extends LightningElement {
     updateList(){
         this.displayedOpps = [];
 
-        // Will need to add filtering logic
-        this.displayedOpps = this.allOpps;
-        this.recordsToDisplay = true;
+        let currentRecord = {};
+
+        if(this.status === 'All'){
+            this.displayedOpps = this.allOpps;
+        }
+        else {
+            for (let i=0; i < this.allOpps.length; i++){
+                currentRecord = this.allOpps[i];
+                if( this.status === 'Open'){
+                    if(!currentRecord.IsClosed) {
+                        this.displayedOpps.push(currentRecord);
+                    }
+                }
+                else if (this.status === 'Closed'){
+                    if(currentRecord.IsClosed) {
+                        this.displayedOpps.push(currentRecord);
+                    }
+                }
+                else if (this.status === 'ClosedWon'){
+                    if(currentRecord.IsWon) {
+                        this.displayedOpps.push(currentRecord);
+                    }
+                }
+                else if (this.status === 'ClosedLost'){
+                    if(!currentRecord.IsWon && currentRecord.isClosed) {
+                        this.displayedOpps.push(currentRecord);
+                    }
+                }
+            }
+        }
+
+        this.recordsToDisplay = this.displayedOpps.length > 0 ? true : false;
     }
 
+    handleChange(event){
+        this.status = event.detail.value;
+        this.updateList();
+    }
 }
