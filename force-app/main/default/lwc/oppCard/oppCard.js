bc42cf2fe35537cdb38d0580c0c426a4a3c69dde
stonypjw <jeremy.white@stonyp.com>
2024-03-06 08:32:14 -0800

    oppCard

---
+++ force-app/main/default/lwc/oppCard/oppCard.js
@@ -0,0 +1,12 @@
+import { LightningElement, api } from 'lwc';
+
+export default class OppCard extends LightningElement {
+
+    //public properties to store opp field values
+    @api name;
+    @api amount;
+    @api stage;
+    @api closedate
+    @api oppId;
+
+}
