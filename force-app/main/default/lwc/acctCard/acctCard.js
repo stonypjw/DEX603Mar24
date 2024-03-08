ef0ea721c50572d42e5d73ebbff517711cb1ffa2
stonypjw <jeremy.white@stonyp.com>
2024-03-08 09:37:36 -0800

    Update acctCard.js

--- force-app/main/default/lwc/acctCard/acctCard.js
+++ force-app/main/default/lwc/acctCard/acctCard.js
@@ -9,6 +9,13 @@ export default class AcctCard extends LightningElement {
     @api phone;
     @api acctId;
 
+        // create a method to dispatch an event with selection information
+        handleSelect() {
+            // create a custom event, and pass the account Id and Name in the event detail
+            const myEvent = new CustomEvent('selected', { detail: { 'prop1': this.acctId, 'prop2': this.name}});
+            this.dispatchEvent(myEvent);
+        }
+
  
 
 
