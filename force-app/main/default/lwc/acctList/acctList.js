40f004c89836ff052e3464acea102dfb097c2a97
stonypjw <jeremy.white@stonyp.com>
2024-03-08 09:47:01 -0800

    MEssageServiceUpdate

--- force-app/main/default/lwc/acctList/acctList.js
+++ force-app/main/default/lwc/acctList/acctList.js
@@ -2,11 +2,18 @@ import { LightningElement, wire } from 'lwc';
 import getTopAccounts from '@salesforce/apex/AccountController.getTopAccounts';
 import RecordModal from 'c/recordModal';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import AccountMC from '@salesforce/messageChannel/AccountMessageChannel__c';
+import { publish, MessageContext } from 'lightning/messageService';
 
 export default class AcctList extends LightningElement {
 
     records;                // propety to hold the account records returned from the wire service
+    selectedId;
+    selectedName;
 
+    // use the wire service to create the MessageContext object required by the publish method
+    @wire(MessageContext)
+    messageContext;
 
     // wire up the getTopAccounts and return the results into a method
     @wire(getTopAccounts)
@@ -14,7 +21,10 @@ export default class AcctList extends LightningElement {
         // check if data was returned
         if (data) {
             this.records = data;
-
+            // publish to the message channel when records first load
+            this.selectedId = this.records[0].Id;
+            this.selectedName = this.records[0].Name;
+            this.sendMessageService(this.selectedId, this.selectedName);
        }
 
         // check if error was returned
@@ -44,4 +54,20 @@ export default class AcctList extends LightningElement {
             }
         });
 }
+
+    // create a method to handle the selected event from acct card
+    handleSelection(event) {
+        this.selectedId = event.detail.prop1;
+        this.selectedName = event.detail.prop2;
+        console.log('acctList: event received with: ' + this.selectedId + ' ' + this.selectedName);
+        // invoke method to publish to the message channel
+        this.sendMessageService(this.selectedId, this.selectedName);
+    }
+
+    // create a method to publish the account ID and Name to the message channel
+    sendMessageService(accountId, accountName) {
+        // invoke the publish method to publish the account info to the message channel
+        publish(this.messageContext, AccountMC, { recordId: accountId, accountName: accountName });
+        console.log('acctList: Published a message with : ' + accountId + ' ' + accountName);
+    }
 }
