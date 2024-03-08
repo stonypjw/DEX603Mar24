d8188ea4cbf979802d78a0dadff5a491e0150a09
stonypjw <jeremy.white@stonyp.com>
2024-03-08 12:15:59 -0800

    Contact

--- force-app/main/default/lwc/acctCard/acctCard.js
+++ force-app/main/default/lwc/acctCard/acctCard.js
@@ -1,4 +1,5 @@
 import { LightningElement, api } from 'lwc';
+import getContactList from '@salesforce/apex/ContactController.getContactList';
 
 
 export default class AcctCard extends LightningElement {
@@ -9,11 +10,40 @@ export default class AcctCard extends LightningElement {
     @api phone;
     @api acctId;
 
+    showContacts = false;
+    loadingContacts = false;
+    contacts;
+
         // create a method to dispatch an event with selection information
         handleSelect() {
             // create a custom event, and pass the account Id and Name in the event detail
             const myEvent = new CustomEvent('selected', { detail: { 'prop1': this.acctId, 'prop2': this.name}});
             this.dispatchEvent(myEvent);
+        }
+
+        displayContacts(){
+            if(this.showContacts){
+                this.showContacts = false;
+            }
+            else {
+                
+                this.loadingContacts = true;
+
+                getContactList({ accountId: this.acctId })
+                    .then((data) => {
+                        this.contacts = data;
+                        this.showContacts = true;
+                    })
+                    .catch((error) => {
+                        console.error('Error retrieving contacts...');
+                        this.showContacts = false;
+                    })
+                    .finally(() => {
+                        console.log('Contacts recieved and displaying...');
+                        this.loadingContacts = false;
+                    });
+
+            }
         }
 
  
